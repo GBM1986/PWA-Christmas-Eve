8259f605c0a5a3b0d72d340e3acf6089c472e9c3
GBM1986 <gunnar_bjarni@hotmail.com>
2023-10-26 11:51:56 +0200

    Test

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const countdownElement = document.getElementById('countdown');
+    const christmasEve = new Date('2023-12-24T00:00:00').getTime();
+
+    function updateCountdown() {
+        const now = new Date().getTime();
+        const timeRemaining = christmasEve - now;
+
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+
+        countdownElement.textContent = `${days} days until Christmas Eve`;
+    }
+
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
+});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
+}
