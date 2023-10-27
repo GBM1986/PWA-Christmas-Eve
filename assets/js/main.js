302fd2a79cad9826e706ee23dca9a67a0a8c3e08
GBM1986 <gunnar_bjarni@hotmail.com>
2023-10-27 13:07:28 +0200

    zxczxc

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./assets/js/service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js')
         .then((registration) => {
             console.log('Service Worker registered with scope:', registration.scope);
         })
